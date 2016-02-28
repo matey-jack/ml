6f1d7f61f892ea70412d56c0e8b118d2fc40a40b
Robert J. Will <matey.jack@gmail.com>
2016-02-28 12:26:26 +0100

    refactored rent vs surface vs rooms analysis; started rent vs bux analysis

--- dwh/src/pd-test.py
+++ dwh/src/pd-test.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 
-estates = pd.read_csv('real-estates-utf8.csv')
-estates = estates[['Immobilientyp', 'Stadt Kreis', 'Kaufpreis', 'Mietekalt', 'Wohnfläche', 'Zimmeranzahl']]
+estates = pd.read_csv('miete.csv')
 
+whg = estates[estates['typ'] == 'Wohnung']
+haus = estates[estates['typ'] == 'Haus']
+
+print(whg.groupby('weekday').aggregate(count))
 
--- dwh/src/real-estate-convert.py
+++ dwh/src/real-estate-convert.py
@@ -2,44 +2,41 @@
 # -*- coding: utf-8 -*-
 import pandas as pd
 import csv
+import re
 
-with open('real-estates-utf8.csv', 'r', encoding="utf8") as f:
-    with open('miete.csv', 'w', encoding="utf8") as miete_f:
-        with open('kauf.csv', 'r', encoding="utf8") as kauf_f:
-            rows = csv.reader(f, quotechar='"', delimiter=',')
-            header = next(rows)
-            print(header)
+re_numeric = re.compile(r'\d*\.?\d*')
+
+with open('real-estates-utf8.csv', newline='', encoding="utf8") as f:
+    rows = csv.reader(f, quotechar='"', delimiter=',')
+    header = next(rows)
+    print(header)
+    with open('miete.csv', 'w', newline='', encoding="utf8") as miet_f:
+        miet_csv = csv.writer(miet_f)
+        miet_csv.writerow(['typ', 'kreis', 'miete', 'fläche', 'zimmer'])
+        with open('kauf.csv', 'w', newline='', encoding="utf8") as kau_f:
+            kauf_csv = csv.writer(kau_f)
+            kauf_csv.writerow(['typ', 'kreis', 'preis', 'fläche', 'zimmer'])
             for row in rows:
                 typ = row[1].replace('Miete', '').replace('Kauf', '').strip(' /')
+                ort = row[3]
                 kaufpreis = row[4].replace(',', '.')
                 miete = row[5].replace(',', '.')
-                if kaufpreis:
-                    kauf_summe[typ] = kauf_summe.get(typ, 0) + float(kaufpreis)
-                    kauf_anzahl[typ] = kauf_anzahl.get(typ, 0) + 1
+                fläche = row[6].replace(',', '.')
+                zimmer = row[8].replace(',', '.')
+                if re_numeric.fullmatch(zimmer):
+                    if zimmer and float(zimmer) > 20:
+                        zimmer = ''
+                else:
+                    print("'{}' ".format(zimmer), end='')
+                    zimmer = ''
+
+                # some objects have this data obviously swapped the wrong way; fix it.
+                if typ == 'Wohnung' and miete and float(miete) > 5000:
+                    kaufpreis = miete
+                    miete = ''
+                    
                 if miete:
-                    if float(miete) < 10000 or typ != 'Wohnung':
-                        miet_summe[typ] = miet_summe.get(typ, 0) + float(miete)
-                        miet_anzahl[typ] = miet_anzahl.get(typ, 0) + 1
-                    else:
-                        print(row)
-    
-print(u'durchschnittliche Kaufpreise: ')
-print(u'{0:20} {1:8} {2:15}'.format('Immotyp', 'Fallzahl', u'⌀ Preis'))
-for typ in sorted(kauf_summe.keys(), key = lambda typ: kauf_anzahl[typ]):
-    print(u'{0:20} {1:8d} {2:12.2f}'.format(typ, kauf_anzahl[typ], kauf_summe[typ] / kauf_anzahl[typ]))
-
-print(u'\ndurchschnittliche Mieten: ')
-print(u'{0:20} {1:8} {2:11}'.format('Immotyp', 'Fallzahl', u'⌀ Preis'))
-for typ in sorted(miet_summe.keys(), key = lambda typ: miet_anzahl[typ]):
-    print(u'{0:20} {1:8d} {2:8.2f}'.format(typ, miet_anzahl[typ], miet_summe[typ] / miet_anzahl[typ]))
-
-
-#%%
-
-import pygal
-
-chart = pygal.Bar()
-chart.add('Kaufpreise', [ kauf_summe[k] / kauf_anzahl[k] for k in kauf_summe.keys() ])
-chart.x_labels = kauf_summe.keys()
-chart.render_to_file('kauf_chart.svg')
-
+                    miet_csv.writerow([typ, ort, miete, fläche, zimmer])
+                if kaufpreis:
+                    kauf_csv.writerow([typ, ort, kaufpreis, fläche, zimmer])
+      
