9015e799ed06d2cef1bd29ee682c2bd38d025c01
Robert J. Will <matey.jack@gmail.com>
2016-03-02 00:30:59 +0100

    polishing the graphs and the code

--- dwh/Backlog.md
+++ dwh/Backlog.md
@@ -2,6 +2,9 @@
    - Städte: vorletztes Feld ist neueste EW-Zahl
    - Landkreis: erstes Zahlenfeld
 
+- perfekt wären natürlich noch Daten zum Bevölkerungswachstum, aber da hatte de-statis nur etwas für den Zeitraum 2002 bis 2011 oder so. Kann ich ja mal angehen, falls nichts spannenderes anliegt...
+
+
 
 D O N E  -  L I S T
 ===================
--- dwh/src/pandas_by_example.py
+++ dwh/src/pandas_by_example.py
@@ -3,4 +3,13 @@ whg['miete_qm'] = whg.miete / whg.fläche    # triggers warning, but works as in
 whg.loc['miete_qm'] = whg.miete / whg.fläche   # triggers warning, no effect
 whg.loc[:,'miete_qm'] = whg.miete / whg.fläche    # suggested by documentation,triggers two warnings, but works as intended
 
+# ggf. probieren, falls es Schwierigkeiten mit dem anderen Axenkonzept gibt
+plot(secondary_y=True)
 
+'''
+xlim/ylim und xticks/yticks muss man vor dem .plot() setzen (sonst wird es ignoriert),
+Legend und Grid muss aber danach kommen (sonst wird es auch ignoriert).
+Eine Fehlermeldung gibt es dabei nur bei der Legende, die wenn zu früh aufgerufen gar
+keine Inhalte findet, die sie zeichnen kann.
+
+'''
