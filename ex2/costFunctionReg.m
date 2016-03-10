c4c74abb80505cb6e8514462aea6bd8c5aca01ab
Robert J. Will <matey.jack@gmail.com>
2016-03-10 08:37:08 +0100

    copy vectorized cost function from previous exercise

--- ex2/costFunctionReg.m
+++ ex2/costFunctionReg.m
@@ -4,9 +4,7 @@ function [J, grad] = costFunctionReg(theta, X, y, lambda)
 %   theta as the parameter for regularized logistic regression and the
 %   gradient of the cost w.r.t. to the parameters. 
 
-% Initialize some useful values
-m = length(y); % number of training examples
-
+m = length(y);
 
 J = -mean(y .* log(sigmoid(X * theta)) + (1.-y) .* log(1 .- sigmoid(X * theta))) ...
     + sum(theta(2:end) .^ 2) * lambda/m/2 ;
