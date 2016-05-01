de55a2f30cf9dc67c63ade3c7288287bcc4de825
Robert J. Will <matey.jack@gmail.com>
2016-05-01 17:35:52 +0200

    ex8 task 1 done

--- ex8/estimateGaussian.m
+++ ex8/estimateGaussian.m
@@ -11,8 +11,8 @@ function [mu sigma2] = estimateGaussian(X)
 [m, n] = size(X);
 
 % You should return these values correctly
-mu = zeros(n, 1);
-sigma2 = zeros(n, 1);
+mu = mean(X, 1);
+sigma2 = meansq(X .- mu);
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Compute the mean of the data and the variances
@@ -20,17 +20,5 @@ sigma2 = zeros(n, 1);
 %               the data for the i-th feature and sigma2(i)
 %               should contain variance of the i-th feature.
 %
-
-
-
-
-
-
-
-
-
-
-% =============================================================
-
 
 end
