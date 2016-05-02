5aed43e94801f42649d8854f70237d3d1b120820
Robert Will <robert.will@immobilienscout24.de>
2016-05-02 18:38:07 +0200

    finish ex8 and add my own movie ratings

--- ex8/cofiCostFunc.m
+++ ex8/cofiCostFunc.m
@@ -13,11 +13,13 @@ Theta = reshape(params(num_movies*num_features+1:end), ...
                 num_users, num_features);
             
 % You need to return the following values correctly
-J = sum(sumsq((X*Theta' - Y) .* R))/2;
+J = sum(sumsq((X*Theta' - Y) .* R))/2 ... 
+  + sum(sum(X .^ 2))*lambda/2 ...
+  + sum(sum(Theta .^ 2))*lambda/2;
 
-X_grad = ((X*Theta' - Y) .* R) * Theta;
+X_grad = ((X*Theta' - Y) .* R) * Theta  +  lambda * X;
 
-Theta_grad = ((X*Theta' - Y) .* R)' * X;
+Theta_grad = ((X*Theta' - Y) .* R)' * X  +  lambda * Theta;
 
 grad = [X_grad(:); Theta_grad(:)];
 
